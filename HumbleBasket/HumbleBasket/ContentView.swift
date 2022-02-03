87e16c38028702bb3060c88058275e0e9be53ab6
Karen Salinas <ksalinas3@csub.edu>
2022-02-02 22:39:36 -0800

    HumbleBasket

---
+++ HumbleBasket/HumbleBasket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HumbleBasket
+//
+//  Created by Karen Salinas on 2/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HumbleBasket/HumbleBasket/HumbleBasketApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumbleBasketApp.swift
+//  HumbleBasket
+//
+//  Created by Karen Salinas on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct HumbleBasketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
